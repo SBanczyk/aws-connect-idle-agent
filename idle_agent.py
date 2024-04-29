43d2788bb8f464d02d528bba167b6773120539ad
Szymon Bańczyk <sbanczyk02@interia.pl>
2024-04-30 00:13:55 +0200

    Initial commit.

--- idle_agent.py
+++ idle_agent.py
@@ -24,52 +24,43 @@ def count_connections():
     return len(result.stdout.decode("utf-8").strip().split("\n")) - 1
 
 
-def get_last_connection_timestamp():
-    result = subprocess.run(
-        ["ss", "-t", "-H", "-n", f"dport = {PORT}"], capture_output=True
-    )
-    connections = result.stdout.decode("utf-8").strip().split("\n")
-    last_connected_timestamp = None
-    for conn in connections[1:]:
-        conn_info = conn.split()
-        timestamp_str = conn_info[-1]
-        last_connected_timestamp = datetime.datetime.strptime(
-            timestamp_str, "%Y-%m-%d %H:%M:%S"
-        )
-    return last_connected_timestamp
-
-
 async def send_message():
     session = boto3.Session(
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         region_name=REGION_NAME,
     )
-    sqs = session.resource("sqs")
-    instance_idle = instance_idle_pb2.InstanceIdle()
-    instance_idle.timestamp.GetCurrentTime()
-    instance_idle.instance_id = INSTANCE_ID
-    last_connected_timestamp = get_last_connection_timestamp()
-    timestamp = datetime.datetime.now()
+    last_connected_timestamp = None
+    if count_connections() > 0:
+        last_connected_timestamp = datetime.datetime.now()
     if (
         last_connected_timestamp is None
-        or (timestamp - last_connected_timestamp.ToDatetime()).total_seconds()
+        or (
+            datetime.datetime.now() - last_connected_timestamp.ToDatetime()
+        ).total_seconds()
         >= IDLE_TIME
     ):
-        last_connected_timestamp = timestamp
-    instance_idle.last_connected_timestamp.FromDatetime(last_connected_timestamp)
-    encoded_message = base64.b64encode(instance_idle.SerializeToString()).decode(
-        "ascii"
-    )
-    queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
-    queue.send_message(
-        MessageBody=encoded_message,
-        MessageGroupId="idle-agent",
-        MessageDeduplicationId=str(time.time_ns()),
-        MessageAttributes={
-            "origin": {"StringValue": "aws-connect-idle-agent", "DataType": "String"}
-        },
-    )
+        sqs = session.resource("sqs")
+        instance_idle = instance_idle_pb2.InstanceIdle()
+        instance_idle.timestamp.GetCurrentTime()
+        instance_idle.instance_id = INSTANCE_ID
+        if last_connected_timestamp is not None:
+            instance_idle.last_connected_timestamp.FromDatetime(last_connected_timestamp)
+        encoded_message = base64.b64encode(instance_idle.SerializeToString()).decode(
+            "ascii"
+        )
+        queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
+        queue.send_message(
+            MessageBody=encoded_message,
+            MessageGroupId="idle-agent",
+            MessageDeduplicationId=str(time.time_ns()),
+            MessageAttributes={
+                "origin": {
+                    "StringValue": "aws-connect-idle-agent",
+                    "DataType": "String",
+                }
+            },
+        )
 
 
 async def main():
@@ -77,7 +68,4 @@ async def main():
         await send_message()
         await asyncio.sleep(CHECK_INTERVAL)
 
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+asyncio.run(main())
