a2549c3ea38c2870ea9be3093a663a7cf36282b9
Szymon Bańczyk <sbanczyk02@interia.pl>
2024-05-07 11:31:11 +0200

    Initial commit.

--- idle_agent.py
+++ idle_agent.py
@@ -30,39 +30,36 @@ async def send_message():
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         region_name=REGION_NAME,
     )
-    last_connected_timestamp = None
     if count_connections() > 0:
         last_connected_timestamp = datetime.datetime.now()
-    if (
-        last_connected_timestamp is None
-        or (
+        message_sent = False
+        if (
             datetime.datetime.now() - last_connected_timestamp.ToDatetime()
-        ).total_seconds()
-        >= IDLE_TIME
-    ):
-        sqs = session.resource("sqs")
-        instance_idle = instance_idle_pb2.InstanceIdle()
-        instance_idle.timestamp.GetCurrentTime()
-        instance_idle.instance_id = INSTANCE_ID
-        if last_connected_timestamp is not None:
-            instance_idle.last_connected_timestamp.FromDatetime(
-                last_connected_timestamp
+        ).total_seconds() >= IDLE_TIME and not message_sent:
+            sqs = session.resource("sqs")
+            instance_idle = instance_idle_pb2.InstanceIdle()
+            instance_idle.timestamp.GetCurrentTime()
+            instance_idle.instance_id = INSTANCE_ID
+            if last_connected_timestamp is not None:
+                instance_idle.last_connected_timestamp.FromDatetime(
+                    last_connected_timestamp
+                )
+            encoded_message = base64.b64encode(
+                instance_idle.SerializeToString()
+            ).decode("ascii")
+            queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
+            queue.send_message(
+                MessageBody=encoded_message,
+                MessageGroupId="idle-agent",
+                MessageDeduplicationId=str(time.time_ns()),
+                MessageAttributes={
+                    "origin": {
+                        "StringValue": "aws-connect-idle-agent",
+                        "DataType": "String",
+                    }
+                },
             )
-        encoded_message = base64.b64encode(instance_idle.SerializeToString()).decode(
-            "ascii"
-        )
-        queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
-        queue.send_message(
-            MessageBody=encoded_message,
-            MessageGroupId="idle-agent",
-            MessageDeduplicationId=str(time.time_ns()),
-            MessageAttributes={
-                "origin": {
-                    "StringValue": "aws-connect-idle-agent",
-                    "DataType": "String",
-                }
-            },
-        )
+            message_sent = True
 
 
 async def main():
